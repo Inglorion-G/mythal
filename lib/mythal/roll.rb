f3c783f0995ee701978cc4ec3363cd2c49c8ea77
George Groh <george.groh@gmail.com>
2017-10-22 14:13:43 -0700

    ternary for compatibility

--- lib/mythal/roll.rb
+++ lib/mythal/roll.rb
@@ -22,7 +22,7 @@ module Mythal
 
       @number_of_dice = match_data[1].to_i
       @denomination = match_data[2].to_i
-      @bonus = match_data[3]&.to_i || 0
+      @bonus = match_data[3] ? match_data[3].to_i : 0
 
       return "can't roll #{dice_string}" unless valid_denomination?
       return "can only roll #{min_dice} - #{max_dice} dice" unless valid_number_of_dice?
